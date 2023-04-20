2c06052260cae1e2d6c5e62a6c869c22e47d4c07
VantaBlack9000 <timmy.gerlach@student.unisg.ch>
2023-04-20 15:59:12 +0200

    Commit 1.3 Graphs, running-time, lengths displayed

--- views.py
+++ views.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, make_response
 import numpy as np
 from scipy import spatial
 import pandas as pd
 import matplotlib.pyplot as plt
 from sko.ACA import ACA_TSP
 from sko.GA import GA_TSP
+import io
+import base64
+import time
 
 views = Blueprint(__name__, "views")
 
@@ -42,11 +45,16 @@ def calculate_aco():
         return sum([distance_matrix[routine[i % num_points], routine[(i + 1) % num_points]] for i in range(num_points)])
     
     #ant colony optimization
+    start_time_aco = time.time()
+
     aca = ACA_TSP(func=cal_total_distance, n_dim=num_points,
                size_pop = size_pop, max_iter=max_iter,
               distance_matrix=distance_matrix)
 
     best_x, best_y = aca.run()
+
+    end_time_aco = time.time()
+    total_time_aco = end_time_aco - start_time_aco
 
     fig, ax = plt.subplots(1, 2)
     best_points_ = np.concatenate([best_x, [best_x[0]]])
@@ -54,11 +62,17 @@ def calculate_aco():
     ax[0].plot(best_points_coordinate[:, 0], best_points_coordinate[:, 1], 'o-r')
     pd.DataFrame(aca.y_best_history).cummin().plot(ax=ax[1])
     plt.title("ACO Output & Performance")
-    plt.savefig("C:/Users/Timmy Gerlach/Documents/Uni/Master/Masterarbeit/App/static/pictures/aco.png")
+    plt.savefig("static/pictures/aco.png")
+    best_distance_aco = best_y
     
     #genetic algorithm
+    start_time_ga =time.time()
+
     ga_tsp = GA_TSP(func=cal_total_distance, n_dim=num_points, size_pop = size_pop, max_iter = max_iter, prob_mut = prob_mut)
     best_points, best_distance = ga_tsp.run()
+
+    end_time_ga= time.time()
+    total_time_ga= end_time_ga - start_time_ga
 
     fig, ax = plt.subplots(1, 2)
     best_points_ = np.concatenate([best_points, [best_points[0]]])
@@ -66,10 +80,11 @@ def calculate_aco():
     ax[0].plot(best_points_coordinate[:, 0], best_points_coordinate[:, 1], 'o-r')
     ax[1].plot(ga_tsp.generation_best_Y)
     plt.title("GA Output & Performance")
-    plt.savefig("C:/Users/Timmy Gerlach/Documents/Uni/Master/Masterarbeit/App/static/pictures/ga.png")
+    plt.savefig("static/pictures/ga.png")
+    best_distance_ga = best_distance[0]
 
     #return frontend and variables
-    return render_template("calculator.html", num_points=num_points, max_iter=max_iter, size_pop=size_pop, prob_mut=prob_mut, plot_url="C:/Users/Timmy Gerlach/Documents/Uni/Master/Masterarbeit/App/static/pictures/aco.png")
+    return render_template("calculator.html", num_points=num_points, max_iter=max_iter, size_pop=size_pop, prob_mut=prob_mut, plot_url_aco="static/pictures/aco.png", plot_url_ga="static/pictures/ga.png", total_time_aco=total_time_aco, total_time_ga=total_time_ga, best_distance_aco=best_distance_aco, best_distance_ga=best_distance_ga)
 
 @views.route("/contact/")
 def contact():
