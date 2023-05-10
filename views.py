c5577c29477229e52aacf84cd649da77dfc88380
VantaBlack9000 <timmy.gerlach@student.unisg.ch>
2023-05-10 18:10:20 +0200

    Commit 1.8 Markers Implemented, algorithm csv data

--- views.py
+++ views.py
@@ -11,6 +11,7 @@ import time
 import folium
 import os
 from werkzeug.utils import secure_filename
+import openrouteservice as ors
 
 views = Blueprint(__name__, "views")
 
@@ -96,10 +97,10 @@ m = folium.Map(location=[47.4244818, 9.3767173])
 # set the iframe width and height
 m.get_root().width = "800px"
 m.get_root().height = "600px"
-iframe = m.get_root()._repr_html_()
+
 
 #View and method for uploading and diplaying the map
-@views.route("/csv-calculator/", methods=("POST", "GET"))
+@views.route("/csv-calculator/", methods=["POST", "GET"])
 def upload_csv():
 
     if request.method == 'POST':
@@ -107,30 +108,111 @@ def upload_csv():
         data_filename = secure_filename(uploaded_df.filename)
         uploaded_df.save(os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename))
         session['uploaded_data_file_path'] = os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename)
-        return render_template('csv_calc.html', iframe=iframe)
+        return render_template('csv_calc.html')
     
     else:
-        flash("Oops.. this didnt work. please make sure you're using the correct datatype and your connection is stable.")
-    
-#view and method for showing data
+        return render_template("csv_calc.html")
+
+#view and method for showing data table in new tab
 @views.route("/csv-calculator-data/", methods=("POST","GET"))
 def show_data():
     data_file_path = session.get("uploaded_data_file_path", None)
-    uploaded_df = pd.read_csv(data_file_path)
+    uploaded_df = pd.read_csv(data_file_path, delimiter=";")
     uploaded_df_html = uploaded_df.to_html()
     return render_template('show_csv_data.html', data_var = uploaded_df_html)
 
-"""@views.route("/csv-calculator/")
-def plot_coords():
-    df_path = session.get("uploaded_data_file_path", None)
-    df = pd.read_csv(df_path)
+#view and method for plotting the provided coords
+@views.route("/plotted-data/", methods=["POST", "GET"])
+def plot_csv():
 
-    for index, row in df.iterrows():
+    data_file_path = session.get("uploaded_data_file_path", None)
+    coords = pd.read_csv(data_file_path, delimiter=";")
+    
+    lat = coords["lat"].astype(float).to_list()
+    long = coords["long"].astype(float).to_list() 
+    points = []
+
+    for i in range(len(lat)):
+        points.append([lat[i], long[i]])
+
+    line = folium.PolyLine(points, color = "blue", dash_array="5", opacity="85", tooltip="Transit Route 101")
+    marker_group = folium.FeatureGroup(name = "CSV Data")
+
+    for index, row in coords.iterrows():
         lat = row["lat"]
         long = row["long"]
+        marker = folium.Marker(location = [lat, long])
+        marker_group.add_child(marker)
 
-        folium.Marker(location=[lat, long]).add_to(m)"""
+    m.add_child(marker_group)
+    m.add_child(line)
+
+    iframe = m.get_root()._repr_html_()
+    return render_template("csv_calc.html", iframe=iframe)
+
+@views.route("/distances-csv/", methods=["POST", "GET"])
+def calculate_csv_distance():
+
+    if request.method == "POST":
+        num_points = int(request.form["num_points"])
+        max_iter = int(request.form["max_iter"])
+        size_pop = int(request.form["size_pop"])
+        prob_mut = int(request.form["prob_mut"])
+    else:
+        num_points = 10
+        max_iter = 200
+        size_pop = 50
+        prob_mut = 1
+
+    data_file_path = session.get("uploaded_data_file_path", None)
+    coords = pd.read_csv(data_file_path, delimiter=";")
+
+    points_coordinate = np.array(coords[["lat", "long"]])
+    distance_matrix = spatial.distance.cdist(points_coordinate, points_coordinate, metric='euclidean')
+
+    def cal_total_distance(routine):
+        num_points, = routine.shape
+        return sum([distance_matrix[routine[i % num_points], routine[(i + 1) % num_points]] for i in range(num_points)])
     
+    #ant colony optimization
+    start_time_aco = time.time()
+
+    aca = ACA_TSP(func=cal_total_distance, n_dim=num_points,
+               size_pop = size_pop, max_iter=max_iter,
+              distance_matrix=distance_matrix)
+
+    best_x, best_y = aca.run()
+
+    end_time_aco = time.time()
+    total_time_aco = end_time_aco - start_time_aco
+
+    fig, ax = plt.subplots(1, 2)
+    best_points_ = np.concatenate([best_x, [best_x[0]]])
+    best_points_coordinate = points_coordinate[best_points_, :]
+    ax[0].plot(best_points_coordinate[:, 0], best_points_coordinate[:, 1], 'o-r')
+    pd.DataFrame(aca.y_best_history).cummin().plot(ax=ax[1])
+    plt.title("ACO Output & Performance")
+    best_distance_aco = best_y
+    
+    #genetic algorithm
+    start_time_ga =time.time()
+
+    ga_tsp = GA_TSP(func=cal_total_distance, n_dim=num_points, size_pop = size_pop, max_iter = max_iter, prob_mut = prob_mut)
+    best_points, best_distance = ga_tsp.run()
+
+    end_time_ga= time.time()
+    total_time_ga= end_time_ga - start_time_ga
+
+    fig, ax = plt.subplots(1, 2)
+    best_points_ = np.concatenate([best_points, [best_points[0]]])
+    best_points_coordinate = points_coordinate[best_points_, :]
+    ax[0].plot(best_points_coordinate[:, 0], best_points_coordinate[:, 1], 'o-r')
+    ax[1].plot(ga_tsp.generation_best_Y)
+    plt.title("GA Output & Performance")
+    best_distance_ga = best_distance[0]
+
+    return render_template("csv_calc.html", max_iter=max_iter, num_points=num_points, size_pop=size_pop, prob_mut=prob_mut, total_time_aco=total_time_aco, total_time_ga=total_time_ga, best_distance_aco=best_distance_aco, best_distance_ga=best_distance_ga )
+
 @views.route("/contact/")
 def contact():
     return render_template("contact.html")
