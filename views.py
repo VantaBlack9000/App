1c33c4187d068777e1f1f3a73f900c9f30685019
VantaBlack9000 <timmy.gerlach@student.unisg.ch>
2023-06-21 18:31:57 +0200

    Added adress functionality

--- views.py
+++ views.py
@@ -15,6 +15,10 @@ import os
 from werkzeug.utils import secure_filename
 import openrouteservice as ors
 import requests
+import geopy as gp
+from geopy.geocoders import Nominatim
+import geopandas as gpd
+import json
 
 API_KEY = "5b3ce3597851110001cf6248c09bb9f319ff486dbaae400d6f00a30d"
 client = ors.Client(key=API_KEY)
@@ -133,16 +137,16 @@ m.get_root().height = "600px"
 #View and method for uploading and diplaying the map
 @views.route("/csv-calculator/", methods=["POST", "GET"])
 def upload_csv():
-
+    iframe = m.get_root()._repr_html_()
     if request.method == 'POST':
         uploaded_df = request.files['uploaded-file']
         data_filename = secure_filename(uploaded_df.filename)
         uploaded_df.save(os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename))
         session['uploaded_data_file_path'] = os.path.join(current_app.config['UPLOAD_FOLDER'], data_filename)
-        return render_template('csv_calc.html')
+        return render_template('csv_calc.html', iframe = iframe)
     
     else:
-        return render_template("csv_calc.html")
+        return render_template("csv_calc.html", iframe=iframe)
 
 #view and method for showing data table in new tab
 @views.route("/csv-calculator-data/", methods=("POST","GET"))
@@ -158,14 +162,39 @@ def plot_csv():
 
     data_file_path = session.get("uploaded_data_file_path", None)
     coords = pd.read_csv(data_file_path, delimiter=";")
-    
-    if coords["lat"].dtype == float and coords["long"].dtype == float:
+
+    #converting adress to geo coords
+    if coords['lat'].isnull().all() and coords['long'].isnull().all():
+
+        locator = Nominatim(user_agent="TSP_application_thesis")
+
+        for index, row in coords.iterrows():
+            address = f"{row['number']} ,{row['street']}, {row['city']}, {row['country']}"
+            location = locator.geocode(address)
+            coords.at[index, 'lat'] = location.latitude
+            coords.at[index, 'long'] = location.longitude
+
+            if coords["lat"].dtype == float and coords["long"].dtype == float:
+                lat = coords["lat"].tolist()
+                long = coords["long"].tolist()
+
+            else: 
+                lat = coords["lat"].astype(float).tolist()
+                long = coords["long"].astype(float).tolist()
+
+    #if coordinates were provided and are in float type
+    elif coords["lat"].dtype == float and coords["long"].dtype == float:
         lat = coords["lat"].tolist()
         long = coords["long"].tolist()
+
+    #if coordinates were provided but are not in float type 
     else:
         lat = coords["lat"].astype(float).tolist()
         long = coords["long"].astype(float).tolist()
 
+    json_coords = coords.to_json()
+    session["json_coords"] = json_coords
+    
     points = []
     for i in range(len(lat)):
         points.append([lat[i], long[i]])
@@ -175,6 +204,7 @@ def plot_csv():
     for i in range(len(lat)):
         points_ors.append([long[i], lat[i]])
 
+    #defining the top value of the table as starting point
     starting_point_coords = points[0]
 
     #plotting a default route
@@ -184,7 +214,7 @@ def plot_csv():
     #convert back to folium lat, long format
     route_coords = [[coord[1], coord[0]] for coord in route_coords]
 
-    default_route = folium.PolyLine(locations=route_coords, color="blue")
+    #default_route = folium.PolyLine(locations=route_coords, color="blue")
     marker_group = folium.FeatureGroup(name = "CSV Data")
     starting_point = folium.Marker(location = starting_point_coords, icon = folium.Icon(color="red"))
     for index, row in coords.iterrows():
@@ -194,27 +224,28 @@ def plot_csv():
             marker = folium.Marker(location = [lat, long])
             marker_group.add_child(marker)
 
-    m.add_child(default_route)
+    #m.add_child(default_route)
     m.add_child(starting_point)
     m.add_child(marker_group)
 
     iframe = m.get_root()._repr_html_()
     return render_template("csv_calc.html", iframe=iframe)
 
+@views.route("/add-customers/", methods = ["POST", "GET"])
+def manually_add_customers():
+    return render_template("csv_calc.html")
+
 @views.route("/distances-csv/", methods=["POST", "GET"])
 def calculate_csv_distance():
 
-    if request.method == "POST":
-        max_iter = int(request.form["max_iter"])
-        size_pop = int(request.form["size_pop"])
-        prob_mut = int(request.form["prob_mut"])
-    else:
-        max_iter = 200
-        size_pop = 50
-        prob_mut = 1
+    max_iter = 200
+    size_pop = 50
+    prob_mut = 1
 
     data_file_path = session.get("uploaded_data_file_path", None)
-    coords = pd.read_csv(data_file_path, delimiter=";")
+    #coords = pd.read_csv(data_file_path, delimiter=";")
+    json_coords = session.get("json_coords")
+    coords = pd.read_json(json_coords)
 
     #the number of points is given by the lengths of the coords data frame
     num_points = len(coords.index)
@@ -236,16 +267,6 @@ def calculate_csv_distance():
     def cal_total_distance(routine):
         num_points, = routine.shape
         return sum([distance_matrix_km[routine[i % num_points], routine[(i + 1) % num_points]] for i in range(num_points)])
-    
-    #ant colony optimization
-    start_time_aco = time.time()
-    aca = ACA_TSP(func=cal_total_distance, n_dim=num_points,
-               size_pop = size_pop, max_iter=max_iter,
-              distance_matrix=distance_matrix_km)
-    best_x, best_y = aca.run()
-    end_time_aco = time.time()
-    total_time_aco = end_time_aco - start_time_aco
-    best_distance_aco = best_y
 
     #genetic algorithm
     start_time_ga =time.time()
@@ -256,31 +277,24 @@ def calculate_csv_distance():
     best_distance_ga = best_distance[0]
 
     #rearrangement of the points_coordinate variables to make them store the best tours coordinates
-    best_tour_aco_ors = points_coordinate_ors[np.argsort(best_x)]
     best_tour_ga_ors = points_coordinate_ors[np.argsort(best_points)]
 
     #conveting to lists
-    list_aco_ors = best_tour_aco_ors.tolist()
     list_ga_ors = best_tour_ga_ors.tolist()
 
-    #plotting the best routes
-    response_aco = client.directions(coordinates = list_aco_ors, profile = "driving-car", format="geojson")
+    #plotting the best route calculated by the ga
     response_ga = client.directions(coordinates = list_ga_ors, profile = "driving-car", format="geojson")
-    route_coords_aco = response_aco["features"][0]["geometry"]["coordinates"]
     route_coords_ga = response_ga["features"][0]["geometry"]["coordinates"]
 
-    route_coords_aco = [[coord[1], coord[0]] for coord in route_coords_aco]
     route_coords_ga = [[coord[1], coord[0]] for coord in route_coords_ga]
 
-    best_aco_route = folium.PolyLine(locations=route_coords_aco, color="blue")
     best_ga_route = folium.PolyLine(locations=route_coords_ga, color="red")
 
-    m.add_child(best_aco_route)
     m.add_child(best_ga_route)
 
     iframe = m.get_root()._repr_html_()
 
-    return render_template("csv_calc.html", iframe=iframe, max_iter=max_iter, num_points=num_points, size_pop=size_pop, prob_mut=prob_mut, total_time_aco=total_time_aco, total_time_ga=total_time_ga, best_distance_aco=best_distance_aco, best_distance_ga=best_distance_ga )
+    return render_template("csv_calc.html", iframe=iframe, max_iter=max_iter, num_points=num_points, size_pop=size_pop, prob_mut=prob_mut, total_time_ga=total_time_ga, best_distance_ga=best_distance_ga )
 
 @views.route("/click-calculator")
 def click_calculator():
